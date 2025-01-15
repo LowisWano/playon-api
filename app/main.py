9af58836662255bfb71991062979638e336de764
23000003 <kurtdicdican@gmail.com>
2025-01-15 22:35:16 +0800

    chore: setup

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from typing import Union
-
+from utils.HttpError import *
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -13,3 +13,13 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/error-check")
+def error_check():
+    x = 0
+    try:
+        return {"result": 100 / x}
+    except Exception as e:
+        print(e, "HEY")
+        return BadRequestErrorResponse(error_message=str(e)).as_exception()
